f3d2b13c3dcb2bdbabae3c624cf526890795429b
HuaDeity <64831499+HuaDeity@users.noreply.github.com>
2023-04-19 12:20:44 +0800

    fix bash to zsh

--- zsh-osx-autoproxy.plugin.zsh
+++ zsh-osx-autoproxy.plugin.zsh
@@ -2,38 +2,38 @@
 
 # 定义设置代理环境变量的函数
 set_proxy_env() {
-    local proxy_type=$1
-    local env_var_name=$2
-    local proxy_info
+    typeset proxy_type=$1
+    typeset env_var_name=$2
+    typeset proxy_info
 
-    if [ "$proxy_type" == "socks" ]; then
+    if [ "$proxy_type" = "socks" ]; then
         proxy_info=$(networksetup -getsocksfirewallproxy "$network_service")
-    elif [ "$proxy_type" == "https" ]; then 
+    elif [ "$proxy_type" = "https" ]; then 
         proxy_info=$(networksetup -getsecurewebproxy "$network_service")
     else
         proxy_info=$(networksetup -getwebproxy "$network_service") 
     fi
 
-    local proxy_server=$(echo "$proxy_info" | grep -Eo '^Server: .*' | sed -E 's/^Server: //')
-    local proxy_port=$(echo "$proxy_info" | grep -Eo '^Port: .*' | sed -E 's/^Port: //')
-    local proxy_enabled=$(echo "$proxy_info" | grep -Eo '^Enabled: .*' | sed -E 's/^Enabled: //')
+    typeset proxy_server=$(echo "$proxy_info" | grep -Eo '^Server: .*' | sed -E 's/^Server: //')
+    typeset proxy_port=$(echo "$proxy_info" | grep -Eo '^Port: .*' | sed -E 's/^Port: //')
+    typeset proxy_enabled=$(echo "$proxy_info" | grep -Eo '^Enabled: .*' | sed -E 's/^Enabled: //')
 
-    if [ "$proxy_enabled" == "Yes" ]; then
-        if [ "$proxy_type" == "socks" ]; then
+    if [ "$proxy_enabled" = "Yes" ]; then
+        if [ "$proxy_type" = "socks" ]; then
             export "$env_var_name=socks5://$proxy_server:$proxy_port"
         else
             export "$env_var_name=http://$proxy_server:$proxy_port"
         fi
     fi
 
-    if [ "$proxy_type" == "http" ]; then
-        local bypass_domains=$(networksetup -getproxybypassdomains "$network_service" | tr '\n' ',')
+    if [ "$proxy_type" = "http" ]; then
+        typeset bypass_domains=$(networksetup -getproxybypassdomains "$network_service" | tr '\n' ',')
         export no_proxy="${bypass_domains%,}"
     fi
 }
 
 # 获取当前活动网络服务
-network_service=$(networksetup -listallnetworkservices | grep -E 'Wi-Fi|AirPort|Ethernet' | head -n 1)
+network_service=$(networksetup -listallnetworkservices | grep -E 'Wi-Fi|AirPort|Ethernet' | head -1)
 
 proxy() {
     # 设置 HTTP、HTTPS 和 SOCKS 代理环境变量
