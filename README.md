056cac999fe1f7094d9790fffd6be097cd47a513
HuaDeity <hello@huadeity.com>
2023-09-17 14:55:24 +0800

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # zsh-osx-autoproxy
+
+Auto Set Proxy for macOS zsh
+
+## Requirements
+
+Set system proxy on your macOS
+
+## Install
+
+### In your ~/.zshrc
+
+Simply clone this repository and source the script:
+
+```bash
+git clone https://github.com/HuaDeity/zsh-osx-autoproxy.git
+echo "source ${{q-}PWD/zsh-osx-autoproxy/zsh-osx-autoproxy.zsh}"
+```
+
+Then, enable auto proxy in the current interactive shell:
+
+```bash
+source ./zsh-osx-autoproxy/zsh-osx-autoproxy.zsh
+```
+
+### With a plugin manager
+
+[Oh-my-zsh](https://github.com/robbyrussell/oh-my-zsh)
+
+1. Clone this repository in oh-my-zsh's plugins directory:
+
+```bash
+git clone https://github.com/HuaDeity/zsh-osx-autoproxy.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-osx-autoproxy
+```
+
+2. Activate the plugin in ~/.zshrc:
+
+```bash
+plugins=( [plugins...] zsh-osx-autoproxy)
+```
+
+3. Restart zsh (such as by opening a new instance of your terminal emulator).
+
+## Commands
+
+```bash
+proxy
+noproxy
+```
+
+There are two commands you can use.
+
+## See Also
+
+Waiting for Linux user making bash-linux-autoproxy.
+
+[powershell-windows-autoproxy](https://github.com/HuaDeity/powershell-windows-autoproxy)
+
+## Thanks
+
+Thanks to [zsh-syntax-highlighting](https://github.com/zsh-users/zsh-syntax-highlighting) for the install instruction.
---
+++ zsh-osx-autoproxy.zsh
@@ -0,0 +1,67 @@
+#!/bin/zsh
+
+# 定义设置代理环境变量的函数
+set_proxy_env() {
+    typeset proxy_type=$1
+    typeset env_var_name=$2
+    typeset proxy_info
+
+    if [ "$proxy_type" = "socks" ]; then
+        proxy_info=$(networksetup -getsocksfirewallproxy "$network_service")
+    elif [ "$proxy_type" = "https" ]; then 
+        proxy_info=$(networksetup -getsecurewebproxy "$network_service")
+    else
+        proxy_info=$(networksetup -getwebproxy "$network_service") 
+    fi
+
+    typeset proxy_server=$(echo "$proxy_info" | grep -Eo '^Server: .*' | sed -E 's/^Server: //')
+    typeset proxy_port=$(echo "$proxy_info" | grep -Eo '^Port: .*' | sed -E 's/^Port: //')
+    typeset proxy_enabled=$(echo "$proxy_info" | grep -Eo '^Enabled: .*' | sed -E 's/^Enabled: //')
+
+    if [ "$proxy_enabled" = "Yes" ]; then
+        if [ "$proxy_type" = "git" ]; then
+            git config --global "$env_var_name" "http://$proxy_server:$proxy_port"
+        elif [ "$proxy_type" = "socks" ]; then
+            export "$env_var_name=socks5://$proxy_server:$proxy_port"
+        else
+            export "$env_var_name=http://$proxy_server:$proxy_port"
+        fi
+    elif [ "$proxy_type" = "git" ]; then
+        git config --global --unset "$env_var_name"
+    else
+        unset "$env_var_name"
+    fi
+}
+
+set_no_proxy(){
+    typeset bypass_domains=$(networksetup -getproxybypassdomains "$network_service" | tr '\n' ',')
+    if [[ "$bypass_domains" == *"There aren't any bypass domains"* ]]; then
+        unset no_proxy
+    else
+        export no_proxy="${bypass_domains%,}"
+    fi
+}
+
+# 获取当前活动网络服务
+network_service=$(networksetup -listallnetworkservices | grep -E 'Wi-Fi|AirPort|Ethernet' | head -1)
+
+proxy() {
+    # 设置 HTTP、HTTPS 和 SOCKS 代理环境变量
+    set_proxy_env "http" "http_proxy"
+    set_proxy_env "https" "https_proxy"
+    set_proxy_env "socks" "all_proxy"
+    set_no_proxy
+    set_proxy_env "git" "http.proxy"
+    set_proxy_env "git" "https.proxy"
+}
+
+noproxy() {
+    unset http_proxy
+    unset https_proxy
+    unset all_proxy
+    unset no_proxy
+    git config --global --unset http.proxy
+    git config --global --unset https.proxy
+}
+
+proxy
